309c3551491cb1a7c3536a279bd4872627f612fd
Jestin Rodriguez <40747173+jestinrodriguez@users.noreply.github.com>
2024-05-09 18:53:38 -0700

    zod form and dropdown

--- lib/mongodb/database/models/category.model.ts
+++ lib/mongodb/database/models/category.model.ts
@@ -1,5 +1,10 @@
 import { Schema, model, models } from "mongoose";
 
+export interface ICategory extends Document {
+    _id: string;
+    name: string;
+}
+
 const CategorySchema = new Schema({
     name: {type: String, required: true, unique: true },
 })
