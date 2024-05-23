1907a541fdeb66df285a88f6ccdead0f9542b4db
Jestin Rodriguez <40747173+jestinrodriguez@users.noreply.github.com>
2024-05-23 12:39:21 -0700

    added event details page

--- lib/actions/events.action.ts
+++ lib/actions/events.action.ts
@@ -5,6 +5,13 @@ import { handleError } from "../utils"
 import { connectToDatabase } from "../mongodb/database"
 import User from "../mongodb/database/models/user.model"
 import Event from "../mongodb/database/models/event.model"
+import Category from "../mongodb/database/models/category.model"
+
+const populateEvent = async (query: any) => {
+    return query
+    .populate({ path: 'organizer', model:User, select: '_id firstName lastName'})
+    .populate({ path: 'category', model:Category, select: '_id name'})
+}
 
 export const createEvent = async({ event, userId, path }: CreateEventParams) => {
     try {
@@ -27,5 +34,22 @@ export const createEvent = async({ event, userId, path }: CreateEventParams) => 
         return JSON.parse(JSON.stringify(newEvent));
     } catch (error) {
         handleError(error);   
+    }
+}
+
+export const getEventById = async (eventId: string) => {
+    try {
+        await connectToDatabase();
+        
+        // returns additional data of eventId owner
+        const event = await populateEvent(Event.findById(eventId));
+
+        if(!event){
+            throw new Error("Event not found");
+        }
+
+        return JSON.parse(JSON.stringify(event))
+    } catch (error) {
+        handleError(error);
     }
 }
